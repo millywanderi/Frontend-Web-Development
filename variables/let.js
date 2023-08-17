a3d6e8a84567d61d9671ed6f75a9ade7324abef3
millywanderi <milliwanderi5@gmail.com>
2023-08-16 22:33:43 -0700

    used to reassign value to variables

---
+++ variables/let.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+/*
+  let is used whenever you expect to reassign a variable in a code.
+  One can reassign the variables multiple times.
+*/
+
+let counter = 0;
+
+// reassign the counter variable
+counter = 1;
+
+// reassign again
+counter++;
+
+// print the results
+console.log(counter);
