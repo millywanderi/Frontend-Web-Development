aeb9dd9f9a80ae7814439d00517aab60bfd95bb9
millywanderi <milliwanderi5@gmail.com>
2023-08-16 22:25:38 -0700

    const declaring a variable

---
+++ variables/const.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/*
+  const does not allow redeclaration of a variable.
+  It gona print an error message if you redeclare const
+*/
+
+const newVariable = 29;
+
+// print the variable
+console.log(newVariable);
