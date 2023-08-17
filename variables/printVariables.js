d810211fed3cd919df8b71ba420cbb370ccacb5b
millywanderi <milliwanderi5@gmail.com>
2023-08-16 22:01:00 -0700

    print a string, integer and object variables

---
+++ variables/printVariables.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// string variable
+const myFirstVariable = 'Learning to code gives me superpowers';
+
+// int variable
+const mySecondVariable = 29;
+
+// object variable
+const myThirdVariable = { name: 'Milly', age: 29 };
+
+console.log(myFirstVariable);
+console.log(mySecondVariable);
+console.log(myThirdVariable);
