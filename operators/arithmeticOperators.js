e8151fe143acaa9839f0e143be8de898319556d8
millywanderi <milliwanderi5@gmail.com>
2023-08-17 02:12:16 -0700

    JavaScript arithmetic operators

---
+++ operators/arithmeticOperators.js
@@ -0,0 +1,67 @@
+#!/usr/bin/node
+
+// addition
+const x = 20;
+const y = 30;
+
+const result = x + y;
+
+console.log('The sum is:', result);
+
+// substruction
+const a = 50;
+const b = 30;
+
+const subs = a - b;
+
+console.log('The substruction is:', subs);
+
+// division
+const m = 90;
+const n = 30;
+
+const divide = m / n;
+
+console.log('The division is:', divide);
+
+// multiplication
+const i = 40;
+const j = 40;
+
+const multiply = i * j;
+
+console.log('The multiplication is:', multiply);
+
+// exponent
+const k = 8;
+const l = 8;
+
+const exp = k ** l;
+
+console.log('The exponent is:', exp);
+
+// modulus
+const c = 9;
+const d = 2;
+
+const mod = c % d;
+
+console.log('The modulus is:', mod);
+
+// increment
+let startingNumber = 1;
+
+startingNumber++;
+
+startingNumber++;
+
+console.log('The increment is:', startingNumber);
+
+// decrement
+let endingNumber = 9;
+
+endingNumber--;
+
+endingNumber--;
+
+console.log('The decrement is:', endingNumber);
