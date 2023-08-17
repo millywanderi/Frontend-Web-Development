6b36e7838b34b3808ebeb0add26c7aa49fd9ce40
millywanderi <milliwanderi5@gmail.com>
2023-08-17 02:56:18 -0700

    javascript comparison operators

---
+++ operators/comparisonOperators.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+// === operator
+const num1 = 20;
+const num2 = 20;
+
+const result = num1 === num2;
+
+console.log(result);
+
+const str1 = 'hello';
+const str2 = 'world';
+
+const res = str1 === str2;
+
+console.log(res);
