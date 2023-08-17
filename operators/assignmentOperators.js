9ac4d360c2743030feb0d124fa5661d017895432
millywanderi <milliwanderi5@gmail.com>
2023-08-17 02:23:36 -0700

    javascript assignment operators

---
+++ operators/assignmentOperators.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+
+// equal
+const myVariable = 30 + 20 / 2;
+
+console.log('My variable is:', myVariable);
+
+// += operator
+let x = 10;
+
+x += 5;
+
+console.log(x);
+
+// -= operator
+let m = 20;
+
+m -= 10;
+
+m -= 2;
+
+console.log(m);
+
+// *= operator
+let n = 2;
+
+n *= 3;
+
+console.log(n);
+
+// /= operator
+let p = 30;
+
+p /= 10;
+
+p /= 2;
+
+console.log(p);
