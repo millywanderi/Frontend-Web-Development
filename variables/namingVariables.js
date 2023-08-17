eed1d2c0b038c45eac173bba4857d331aa8987a1
millywanderi <milliwanderi5@gmail.com>
2023-08-16 22:46:00 -0700

    naming JS variables

---
+++ variables/namingVariables.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/*
+  The following is allowed ways of naming variables in JS.
+  Put everything in one word when writing a variable.
+  Do not use special cases like "", '', !.
+  Avoid starting the name of the variable with a number.
+*/
+const yourFirstVariable = 10; // camel case
+const your_first_variable = 10; // snake case
+const YOUR_FIRST_VARIABLE = 10; // constant case
+const yourfirstvariable = 10; // lowercase
+const YourFirstVariable = 10; // pascal case
