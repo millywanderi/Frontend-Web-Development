52505950c8ff3976b5b2ab0a11683121280edabb
millywanderi <milliwanderi5@gmail.com>
2023-08-16 22:20:57 -0700

    var declarations

---
+++ variables/var.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/*
+  var is an oldest version of declaring variables in JS.
+  It is no longer used as it can be redeclared/redefined
+*/
+
+var myVariable = 10;
+var myVariable = 20;
+
+console.log(myVariable);
