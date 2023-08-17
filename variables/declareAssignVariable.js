89b6a515be881720f24147292b6053919f7179d6
millywanderi <milliwanderi5@gmail.com>
2023-08-16 22:13:22 -0700

    declare and assign values to a variable

---
+++ variables/declareAssignVariable.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// declare and assign variable at the same time
+const newVariable = 'JavaScript is awesome';
+
+  // declare a variable
+  let anotherVariable;
+
+// assign value to the variable
+anotherVariable = 29;
+
+// print the variables
+console.log(newVariable);
+console.log(anotherVariable);
