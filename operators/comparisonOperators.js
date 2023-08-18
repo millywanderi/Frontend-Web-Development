156d2b0b7ce2be29f6995baed69262325eaee84c
millywanderi <milliwanderi5@gmail.com>
2023-08-18 01:57:45 -0700

    comparison operators in javascript

--- operators/comparisonOperators.js
+++ operators/comparisonOperators.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
 
+/*
+  The != and the !== is the opposite of == and ===
+  In objects and arrays, you cannot compare different objects and arrays
+  Other comparison operators include <, >, <=, >=
+*/
+
 // === operator
 const num1 = 20;
 const num2 = 20;
@@ -14,3 +20,43 @@ const str2 = 'world';
 const res = str1 === str2;
 
 console.log(res);
+
+// === operator on arrays and objcets
+const myArray = [1, 2, 3, 4, 5];
+const anotherArray = [1, 2, 3, 4, 5];
+
+const compareArray = myArray === myArray;
+const compareAnotherArray = myArray === anotherArray;
+
+console.log(compareArray); // The answer is true
+console.log(compareAnotherArray); // the answer is false
+
+// === on objects
+const myObject = {
+  property1: 'some value',
+  property2: 20,
+};
+
+const anotherObject = {
+  property1: 'some value',
+  property2: 20,
+};
+
+const compareMyObject = myObject === myObject;
+const compareAnotherObject = myObject === anotherObject;
+
+console.log(compareMyObject); // returns true
+console.log(compareAnotherObject); // return false
+
+// == operator. It first converts the variables in same value and then compares them
+const stringValue = '20';
+const numberValue = 20;
+
+const result1 = stringValue == numberValue;
+
+console.log(result1);
+
+// ternary operator
+const result2 = 20 === 20 ? 'values match' : 'values do not match';
+
+console.log(result2); // returns values match
