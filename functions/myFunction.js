6f9162b62b73d13aa60251a83652f5cb2a9b6e14
millywanderi <milliwanderi5@gmail.com>
2023-08-30 12:04:07 +0300

    my first js function

---
+++ functions/myFunction.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Declare the function
+function myFunction() {
+	console.log('hello world, this is my first function');
+}
+// check it's type
+typeof myFunction();
+
+// execute or call or invoke the function 
+myFunction();
