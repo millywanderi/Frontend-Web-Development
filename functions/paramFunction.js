25911bc69a3596b5320dc0102686c9a65b08b4b6
millywanderi <milliwanderi5@gmail.com>
2023-08-30 12:16:27 +0300

    parameters and arguments in js

---
+++ functions/paramFunction.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// declare function and add parameters
+function myFunction(param1, param2) {
+	consloe.log(param1);
+	console.log(param2);
+}
+// add some arguments into the function
+myFunction(20, 'some string');
+
+// call the function, will print 20 for param1 and some string for param2
+myFunction();
