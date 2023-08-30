8ab68bc1fc583cacbf2e855670c5849ca3410b12
millywanderi <milliwanderi5@gmail.com>
2023-08-30 12:29:29 +0300

    ananymous function in js

---
+++ functions/assignVarFunct.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Writing a ananymous function
+const anotherFunction = function () {
+	console.log('another function');
+}
+// check it's type. It returns function
+typeof anotherFunction
+
+// call the function it prints another function
+anotherFunction();
