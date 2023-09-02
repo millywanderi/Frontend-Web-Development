2cf8d6508f497c14ee4624f161c7b7849af2912c
millywanderi <wanderinyambura5@gmail.com>
2023-09-02 14:23:46 +0300

    challenge 3 solution

---
+++ challenge/lesso_3_challenge/challenge_3_solution.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const variable1 = 20;
+const variable2 = '40';
+
+const resultVariable = variable1 + variable2;
+
+console.log(resultVariable);
+// The resultVariable will print "2040", and it is a string.
