d12967a534ebc1a75e3fcc41e85a25e9efc3681f
millywanderi <wanderinyambura5@gmail.com>
2023-09-02 14:28:32 +0300

    challenge 5 solution

---
+++ challenge/lesso_3_challenge/challenge_5_solution.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const myArray = [20, 30, 40];
+
+console.log(myArray[3]);
+
+// The above code is invalid because it attempts to print the value at index 3, but there is no index 3 in this array!
+// Remember, arrays start their indexing at 0, so this array has the following index values:
+
+// Index         0   1    2
+const myArray = [20, 30, 40];
+
+// And therefore, this is the correct way to write this program.
+const myArray = [20, 30, 40];
+
+console.log(myArray[2]);
