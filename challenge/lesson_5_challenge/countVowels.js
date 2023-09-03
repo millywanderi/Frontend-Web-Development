f1cbe3a2f54b5d877bc531edf54f68920bd234a1
millywanderi <wanderinyambura5@gmail.com>
2023-09-03 16:48:43 +0300

    count vowels in lowercase separated by space

---
+++ challenge/lesson_5_challenge/countVowels.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+function getCount(str) {
+    let vowelsCount = 0;
+    const arr = str.split("");
+    for (let i = 0; i < arr.length; i++) {
+        switch(arr[i]) {
+            case 'a':
+                vowelsCount++;
+                break;
+            case 'e':
+                vowelsCount++;
+                break;
+            case 'i':
+                vowelsCount++;
+                break;
+            case 'o':
+                vowelsCount++;
+                break;
+            case 'u':
+                vowelsCount++;
+            
+        }
+    }
+    return vowelsCount;
+}
