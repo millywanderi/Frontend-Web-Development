1e5faa49356abcf191511bc84ea0642449730047
millywanderi <wanderinyambura5@gmail.com>
2023-09-03 16:28:43 +0300

    moved files to lesson 5

--- challenge/reverse.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-// reverse function
-function solution(str) {
-	return str.split('').reverse().join(''); // split converts str to array, reverse reverses the array, and join joins the array together
-}
-console.log(solution('world')); // Output: 'dlrow'
-console.log(solution('word')); // Output: drow
