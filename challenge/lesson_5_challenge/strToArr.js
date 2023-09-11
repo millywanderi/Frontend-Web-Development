b66dd92b67d91905f2978d9cf606289c3dd46866
millywanderi <wanderinyambura5@gmail.com>
2023-09-11 18:47:28 +0300

    converts a str to arr

---
+++ challenge/lesson_5_challenge/strToArr.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Write a function to split a string and convert it into an array of words.
+
+function stringToArray(string){
+    const arr = string.split(' '); // converts the string to an array and separates with spaces
+    return arr;
+}
