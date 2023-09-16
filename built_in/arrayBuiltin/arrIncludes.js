ce007471ba0e5228821fa3c306aa6a48bf95f1a5
millywanderi <wanderinyambura5@gmail.com>
2023-09-16 16:31:34 +0300

    array Includes method in js

---
+++ built_in/arrayBuiltin/arrIncludes.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// determines whether an array includes a certain value among its entries
+// returning true or false as appropriate
+const names = ['Kylie', 'Kinsley', 'Lyle'];
+
+console.log(names.includes('Lyle')); // Output: True
+
+const arr = [1, 2, 3, 4];
+
+console.log(arr.includes(7)) // Output: False
