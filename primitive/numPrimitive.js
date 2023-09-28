9022d6ef473eba45d911159702c74d87318007df
millywanderi <wanderinyambura5@gmail.com>
2023-09-28 12:16:27 +0300

    uses eval function to define the type of the string

---
+++ primitive/numPrimitive.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// uses eval() to define the type of a string
+const s1 = "2 + 2"; // creates a string primitive
+const s2 = new String("2 + 2"); // creates a String object
+console.log(eval(s1)); // returns the number 4
+console.log(eval(s2)); // returns the string "2 + 2"
