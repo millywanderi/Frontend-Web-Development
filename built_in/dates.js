dfac7a1f4a5ea4c97adc1f00b0b4be69be2d7669
millywanderi <wanderinyambura5@gmail.com>
2023-10-02 14:20:39 +0300

    writing dates in javascript

---
+++ built_in/dates.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Writing dates in javascript
+const now = new Date(); // Assign the date to variable now
+console.log(now); // prints the exact date and time
+
+console.log(now.valueOf()); // Prints the number of miliseconds
