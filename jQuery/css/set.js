a4aae3d6bfcbb5ab235e970f854139e6acb54190
millywanderi <wanderinyambura5@gmail.com>
2023-10-04 12:54:41 +0300

    sets css selector

---
+++ jQuery/css/set.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// sets css selector in a single code
+<a href="javascript:void(0);" onClick="$(this).toggleClass('bold');">Toggle class</a>
+
+/*
+The above line of code checks
+.bold {
+	font-weight: bold;
+}
+.blue {
+	color: blue;
+}
+*/
