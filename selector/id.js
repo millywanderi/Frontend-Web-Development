01d6f623e8e5e0968941d71ac2fd1c07dfb4a6c0
millywanderi <wanderinyambura5@gmail.com>
2023-10-04 11:02:13 +0300

    id selector

---
+++ selector/id.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Id selector is used to locate single, unique element using #
+<div id="divTest"></div> // html div element with attribute id
+<script type="text/javascript"> //js script
+$(function() jQuery function
+{
+	$("#divTest").text("Test"); // jQuery selector and method used to set textcontent
+});
+</script>
