f9512ec9add6f6b0fbeeacedb45230d92142e0fb
millywanderi <wanderinyambura5@gmail.com>
2023-11-24 12:46:44 +0300

    determines whether an array includes a certain value among its entries

---
+++ built_in/array/includes.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// determines whether an array includes a certain value among its entries, returning true or false as appropriate.
+const names = ['Steve', 'Lydia', 'Silas'];
+console.log(names.includes('Silas'));  // output: True
