5c6e4d20aad99347dc0c25b824092f18cc8595af
millywanderi <wanderinyambura5@gmail.com>
2023-11-30 18:11:44 +0300

    updated the reduce function to handle a complex calculation

--- built_in/array/reduce.js
+++ built_in/array/reduce.js
@@ -7,3 +7,17 @@ const sumWithInitial = arr.reduce(
     (accumulator, currentValue) => accumulator + currentValue, initialValue,
 );  
 console.log(sumWithInitial);  // otuput: 10
+
+// complex one
+const arr1 = [100, 200, 300, 400];
+let sum = 0;
+for (let i = 0; i < arr1.length; i++) {
+    sum += arr1[i];
+}
+console.log(sum);  // output: 1000
+
+function reduceCallBack(sum, arrItem) {
+    return sum += arrItem;
+}
+const result = arr1.reduce(reduceCallBack, 0);
+console.log(result);  // output: 100
