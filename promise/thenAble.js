aa01ee19ce6c7b4570293a4bab365d724181c7ab
millywanderi <wanderinyambura5@gmail.com>
2024-01-03 14:37:58 +0300

    Implementing an object that has a then method

---
+++ promise/thenAble.js
@@ -0,0 +1,13 @@
+const aThenable = {
+  then(onFulfilled, onRejected) {
+    onFulfilled({
+      // The thenable is fulfilled with another thenable
+      then(onFulfilled, onRejected) {
+        onFulfilled(42);
+      },
+    });
+  },
+};
+
+Promise.resolve(aThenable); // A promise fulfilled with 42
+
