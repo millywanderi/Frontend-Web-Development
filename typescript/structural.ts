8106f3f7e7b67738574735ff21c9e13a2bfe23f8
millywanderi <wanderinyambura5@gmail.com>
2024-01-10 10:09:34 +0300

    type checking focuses on the shape that values have.

---
+++ typescript/structural.ts
@@ -0,0 +1,13 @@
+// One of TypeScript’s core principles is that type checking focuses on the shape that values have.
+interface Point {
+  x: number;
+  y: number;
+}
+ 
+function logPoint(p: Point) {
+  console.log(`${p.x}, ${p.y}`);
+}
+ 
+// logs "12, 26"
+const point = { x: 12, y: 26 };
+logPoint(point);
